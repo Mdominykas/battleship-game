996b48256b09f5a86799726e2cb43803e71bba55
Mdominykas <marmadominykas@gmail.com>
2022-12-28 20:36:59 +0200

    implemented maps and shooting

--- main.py
+++ main.py
@@ -1,4 +1,60 @@
 import tkinter as tk
+import enum
+
+class CellState(enum.Enum):
+	empty_not_hit = 1
+	empty_hit = 2
+	ship_not_hit = 3
+	ship_hit = 4
+
+
+class BoardState:
+	def __init__(self, width, height):
+		self.width = width
+		self.height = height
+		self.state = [[0] * self.width] * self.height
+		self.state = []
+		# print(self.state)
+		for i in range(0, self.width):
+			self.state.append([])
+			for j in range(0, self.height):
+				self.state[-1].append(CellState.empty_not_hit)
+		# self.state[0][0] = CellState.empty_not_hit
+		# self.state[0][1] = CellState.empty_hit
+		# self.state[0][2] = CellState.ship_not_hit
+		# self.state[0][3] = CellState.ship_hit
+	def cell_state(self, x, y):
+		assert((0 <= x) and (x < GameConstants.BOARD_WIDTH) and (0 <= y) and (y < GameConstants.BOARD_HEIGHT))
+		return self.state[y][x]
+	def set_cell_state(self, x, y, new_state):
+		self.state[y][x] = new_state
+	def can_shoot(self, x, y):
+		state = self.cell_state(x, y)
+		return (state == CellState.ship_not_hit) or (state == CellState.empty_not_hit)
+	def shoot(self, x, y):
+		state = self.cell_state(x, y)
+		if(state == CellState.ship_not_hit):
+			self.set_cell_state(x, y, CellState.ship_hit)
+		elif(state == CellState.empty_not_hit):
+			self.set_cell_state(x, y, CellState.empty_hit)
+		else:
+			assert(False)
+	# def hitted_battleship()
+
+	def get_color(self, x, y):
+		state = self.cell_state(x, y)
+		if (state == CellState.empty_not_hit):
+			return ''
+		elif (state == CellState.empty_hit):
+			return 'blue'
+		elif (state == CellState.ship_not_hit):
+			return 'black'
+		elif (state == CellState.ship_hit):
+			return 'red'
+		else:
+			assert(False)
+
+
 
 class BattleshipMap:
 	def __init__(self, canvas, x0, y0, x_max, y_max, height, width, border_width = 3):
@@ -12,14 +68,99 @@ class BattleshipMap:
 		self.height = height
 		self.width = width
 		self.border_width = border_width
+		self.map =  [ [ [0] * width ] * height ]
+		self.state = BoardState(width, height)
 	def draw_board(self):
-		x_len = (self.x_max - self.x0) / self.width
-		y_len = (self.y_max - self.y0) / self.height
+		x_len, y_len = self.compute_lens()
 		for i in range(0, self.height):
 			for j in range(0, self.width):
 				top_left = (self.x0 + j * x_len, self.y0 + i * y_len)
 				bot_right = (self.x0 + (j + 1) * x_len, self.y0 + (i + 1) * y_len)
-				canvas.create_rectangle((top_left, bot_right), width = self.border_width)
+				color = self.state.get_color(j, i)
+				self.canvas.create_rectangle((top_left, bot_right), width = self.border_width, fill=color)
+	def compute_lens(self):
+		x_len = (self.x_max - self.x0) / self.width
+		y_len = (self.y_max - self.y0) / self.height
+		return (x_len, y_len)
+
+	def find_cell(self, x, y):
+		if((x < self.x0) or (self.x_max < x) or (y < self.y0) or (self.y_max < y)):
+			return (-1, -1)
+		x_len, y_len = self.compute_lens()
+		x_num = (x - self.x0) / x_len
+		y_num = (y - self.y0) / y_len
+		return int(x_num), int(y_num)
+
+	def can_shoot(self, x, y):
+		return self.state.can_shoot(x, y)
+
+	def shoot(self, x, y):
+		self.state.shoot(x, y)
+
+
+class GameConstants:
+	# BOARD_HEIGHT = 500
+	# BOARD_WIDTH = 500
+	BOARD_DISPLAY_WIDTH = 500
+	BOARD_DISPLAY_HEIGHT = 500
+	BOARD_DISPLAY_GAP = 100
+	CANVAS_WIDTH = 2 * BOARD_DISPLAY_WIDTH + BOARD_DISPLAY_GAP
+	BOARD_WIDTH = 10
+	BOARD_HEIGHT = 10
+	SHIP_LENGTHS = [5, 4, 3, 3, 2]
+
+
+class Turn(enum.Enum):
+	PLAYER = 0
+	COMPUTER = 1
+
+
+class Gameplay:
+	def __init__(self, player_map, computer_map, starting_turn = Turn.PLAYER):
+		self.current_turn = starting_turn
+		self.player_map = player_map
+		self.computer_map = computer_map
+
+	def process_player_shot(self, x, y):
+		assert((x != -1) and (y != -1))
+		if(self.current_turn != Turn.PLAYER):
+			return
+		if(not self.computer_map.can_shoot(x, y)):
+			return
+		self.computer_map.shoot(x, y)
+		self.computer_map.draw_board()
+		# TODO: pakeisti ejima
+
+
+
+class GameGraphics:
+	def __init__(self, window):
+		self.canvas = tk.Canvas(window, bg="white", width=GameConstants.CANVAS_WIDTH, height=GameConstants.BOARD_DISPLAY_WIDTH)
+		self.canvas.pack()
+
+		self.player_map = BattleshipMap(self.canvas, 0, 0, GameConstants.BOARD_DISPLAY_WIDTH, GameConstants.BOARD_DISPLAY_HEIGHT, GameConstants.BOARD_WIDTH, GameConstants.BOARD_HEIGHT)
+		self.player_map.draw_board()
+
+		self.computer_map = BattleshipMap(self.canvas, GameConstants.BOARD_DISPLAY_WIDTH + GameConstants.BOARD_DISPLAY_GAP, 0, 2 * GameConstants.BOARD_DISPLAY_WIDTH + GameConstants.BOARD_DISPLAY_GAP, GameConstants.BOARD_DISPLAY_HEIGHT, GameConstants.BOARD_WIDTH, GameConstants.BOARD_HEIGHT)
+		self.computer_map.draw_board()
+
+		self.gameplay = Gameplay(self.player_map, self.computer_map)
+
+	def process_mouse_click(self, event):
+		x, y = event.x, event.y
+		if (not ((x >= 0) and (x < GameConstants.CANVAS_WIDTH) and (y >= 0) and (y < GameConstants.BOARD_DISPLAY_HEIGHT))):
+			return 
+		if((GameConstants.BOARD_DISPLAY_WIDTH < x) and (x < GameConstants.BOARD_WIDTH + GameConstants.BOARD_DISPLAY_GAP)):
+			return
+		if(x <= GameConstants.BOARD_DISPLAY_WIDTH):
+			# no gameplay implemented yet
+			return 
+		else:
+			(cellx, celly) = self.computer_map.find_cell(x, y)
+			self.gameplay.process_player_shot(cellx, celly)
+
+
+
 
 
 # def draw_board(canvas, x0, y0, x_max, y_max, height, width, border_width = 3):
@@ -36,11 +177,7 @@ window = tk.Tk()
 greeting = tk.Label(text="Battleship")
 greeting.pack()
 
-canvas = tk.Canvas(window, bg="white", width=500, height=600)
-canvas.pack()
-
-battleship_map = BattleshipMap(canvas, 0, 0, 500, 600, 8, 8)
-battleship_map.draw_board()
+graphics = GameGraphics(window)
 
 # rect = canvas.create_rectangle(((0, 0), (30, 70)), width = 3)
 # draw_board(canvas, 0, 0, 500, 600, 8, 8)
@@ -53,5 +190,5 @@ def action(event):
     print(event.y)
     print()
 
-window.bind("<ButtonPress-1>", action)
+window.bind("<ButtonPress-1>", graphics.process_mouse_click)
 window.mainloop()
